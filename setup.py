a5d975fe9c06ba6e70c10e2b0c040035fe3a1bec
Matthew Tivnan <mtivnan2@aiai-lem.jhmi.edu>
2020-05-27 17:33:09 -0400

    adding a setup script for pip compatibility

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='aiaiTools',
+      version='v0',
+      author='Matthew Tivnan',
+      author_email='mtivnan2@jhmi.edu',
+      url='https://git.lcsr.jhu.edu/istar/aiaiTools',
+      packages=['aiaiTools'],
+      install_requires=['numpy', 'scipy', 'scikit-image']
+      )
